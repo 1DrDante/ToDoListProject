e8385fdfec713c6ce29e5046297a1bdb1e8c31ef
userName23215 <160757951+userName23215@users.noreply.github.com>
2025-10-25 15:13:37 -0600

    Function Fix
    I fixed the function

--- ImportTXT.py
+++ ImportTXT.py
@@ -1,14 +1,16 @@
 #This file purpose is to import elements from the storage.txt as a list.
-tasks = []
-goodToCopy = False
-with open("storage.txt", "r") as file:
-    for line in file:
-        if line.startswith("*"):
-            goodToCopy = not goodToCopy
+def getTasks():
+    tasks = []
+    goodToCopy = False
+    with open("storage.txt", "r") as file:
+        for line in file:
+            if line.startswith("*"):
+                goodToCopy = not goodToCopy
 
-            money = 0
-        
-        if goodToCopy == True:
-            element = line.strip()
-            tasks.append(element)
+                money = 0
+            
+            if goodToCopy == True:
+                element = line.strip()
+                tasks.append(element)
+    return tasks
         
