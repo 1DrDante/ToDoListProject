c604c5237fedd63b8f5ac2f2efcd34ea04c7320b
userName23215 <160757951+userName23215@users.noreply.github.com>
2025-10-25 15:29:43 -0600

    Made ChangeTask

--- ExportTXT.py
+++ ExportTXT.py
@@ -1,1 +1,13 @@
 #This file purpose is to change (add) the to-do list via use input.
+def changeTask(deleteTask, changeTask):
+    theFile = None
+    with open("storage.txt", "r") as file:
+        theFile = file.readlines()
+
+    for line in theFile:
+        if line.__contains__(deleteTask):
+            theFile.remove(line)
+            theFile.append(changeTask + "\n")
+    
+    with open("storage.txt", "w") as file:
+        file.appendlines(theFile)
